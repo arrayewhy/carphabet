4e3683b3dc59573a8be28bdbc2680bd5e981b5d3
Sundrop <random90s@gmail.com>
2026-01-21 02:42:24 +0800

    Changed the way arrow path drawing is handled, according to how the trigger to start arrow drawing is handled by the Barricade for now.

--- Scripts/arrow_path.gd
+++ Scripts/arrow_path.gd
@@ -5,11 +5,13 @@ var _car:Node2D;
 
 var _last_pos:Vector2;
 var _drawing:bool;
-#var _can_draw:bool;
 
 var _arrow_ready:bool;
 
 signal Arrow_Complete;
+
+
+# Functions: Built-in ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
 func _ready() -> void:
@@ -17,7 +19,6 @@ func _ready() -> void:
 	set_process(false);
 	
 	_last_pos = get_global_mouse_position();
-	#_can_draw = true;
 	_arrow_head.hide();
 	
 	$"../../Terrain/Barricades_Start/Barricade".Start_Barricade_Clicked.connect(_SIGNAL_Start_Drawing);
@@ -25,31 +26,8 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	
-	# Remove Arrow Tail according to Progression
-	
-	if _arrow_ready:
-		
-		var points:Array[Vector2];
-		points.assign($"..".points);
-		
-		if points.size() <= 0:
-			$"..".points = [];
-			_arrow_ready = false;
-			set_process(false);
-			_arrow_head.hide();
-			return;
-		
-		for point in points:
-			if point.distance_to(_car.global_position) < 100:
-				points.erase(point);
-		
-		$"..".points = points;
-		
-		return;
-	
 	if _drawing:
-	#if Input.is_action_pressed("Click") && _can_draw:
-	
+		
 		if Input.is_action_just_released("Click"):
 			_drawing = false;
 			_arrow_ready = true;
@@ -68,15 +46,38 @@ func _process(_delta: float) -> void:
 			self.curve.add_point(_last_pos);
 			$"../".points = self.curve.get_baked_points();
 			
-	#if Input.is_action_just_released("Click") && _can_draw:
-		#_can_draw = false;
-		#_arrow_ready = true;
-		#Arrow_Complete.emit();
+		return;
+			
+	# Remove Arrow Tail according to Progression
+	
+	elif _arrow_ready:
+		
+		var points:Array[Vector2];
+		points.assign($"..".points);
+		
+		if points.size() <= 0:
+			$"..".points = [];
+			_arrow_ready = false;
+			set_process(false);
+			_arrow_head.hide();
+			return;
+		
+		for point in points:
+			if point.distance_to(_car.global_position) < 100:
+				points.erase(point);
+		
+		$"..".points = points;
+
+
+# Functions: Signals ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
 func _SIGNAL_Start_Drawing() -> void:
 	_drawing = true;
 	set_process(true);
+
+
+# Functions ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
 func Assign_Car(car:Node2D) -> void:
