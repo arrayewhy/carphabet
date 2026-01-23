d0d66472ffab0e1f1e45c1ff1fc4f2d5abbf1476
Sundrop <random90s@gmail.com>
2026-01-23 22:35:27 +0800

    Made Car at Barricade with multiple Start Points gradually move into frame.

--- Scripts/base.gd
+++ Scripts/base.gd
@@ -15,6 +15,8 @@ signal Move_Car_To_End(car_idx:int, endPoint:Vector2);
 
 func _ready() -> void:
 	
+	var car_tween:Tween
+	
 	var start_barricades:Array[Node] = $"Terrain/Barricades_Start".get_children();
 	
 	for i in start_barricades.size():
@@ -22,19 +24,51 @@ func _ready() -> void:
 		var car:Node2D = _car_prefab.instantiate();
 		car.name = "Car" + str(i);
 		car.set_script(load("res://Scripts/car.gd"));
-		car.position = start_barricades[i].get_node('Node2D').global_position;
 		car.Set_Car_Idx(i);
 		_cars.append(car);
 		self.add_child(car);
 		
 		if i <= 0:
-			print('Car ',i ,' assigned');
-			$Arrow/Path2D.Assign_Car(car);
-			car.Assign_To_Arrow_Path();
+		
+			var start_point_nodes:Array[Node] = start_barricades[i].get_node("Points").get_children();
+			
+			if start_point_nodes.size() <= 1:
+				car.global_position = start_point_nodes[0].global_position;
+			else:
+				
+				# Create Route
+				var route_points:Array[Vector2];
+				for point in start_point_nodes:
+					route_points.append(point.global_position);
+				
+				# Position Car to First Point
+				car.global_position = route_points[0];
+				
+				# Move Car
+				car_tween = create_tween();
+				
+				for r in route_points.size() - 1:
+					var dist:float = car.global_position.distance_to(route_points[r + 1]);
+					if i <= 0:
+						car_tween.tween_property(car, "global_position", route_points[r + 1], dist / car._speed);
+					else:
+						car_tween.chain().tween_property(car, "global_position", route_points[r + 1], dist / car._speed);
+			
+		elif i > 0:
+			
+			car.global_position = start_barricades[i].get_node("Points").get_child(0).global_position;
 		
 		car.Car_Stopped.connect(_SIGNAL_Check_Prog_Next_Scene);
 	
 	$Terrain/Barricades_End/Barricade.End_Barricade_Reached.connect(_Check_Car_On_Barricade_Entry);
+	
+	if car_tween != null:
+		await car_tween.finished;
+
+	# After the First Car is done moving in,
+	# prepare for Path Drawing.
+	$Arrow/Path2D.Assign_Car(_cars[0]);
+	_cars[0].Assign_To_Arrow_Path();
 	
 	Globals.CURRENT_SCENE_TYPE = _scene_type;
 
@@ -56,4 +90,5 @@ func _SIGNAL_Check_Prog_Next_Scene() -> void:
 
 
 func _Check_Car_On_Barricade_Entry(endPoint:Vector2) -> void:
-	Move_Car_To_End.emit(_routes_completed, endPoint);
+	var car_idx:int = _routes_completed;
+	Move_Car_To_End.emit(car_idx, endPoint);
