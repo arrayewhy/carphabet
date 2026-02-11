d2536dbe75661d7cbafddba526974683af17a0d0
Sundrop <random90s@gmail.com>
2026-02-11 13:35:19 +0800

    Add Sound handling to Touch Obstacles

--- Scripts/touch_obstacle.gd
+++ Scripts/touch_obstacle.gd
@@ -5,7 +5,8 @@ var _hovering:bool;
 
 var _spr_array:Array[Sprite2D];
 
-@export var _play_sound:AudioMaster.OBSTACLE_SOUND;
+@export var _touch_sounds:Array[AudioMaster.SOUND_TYPE];
+@export var _col_sounds:Array[AudioMaster.SOUND_TYPE];
 
 
 func _ready() -> void:
@@ -17,6 +18,8 @@ func _ready() -> void:
 	
 	$Area2D.mouse_entered.connect(_Hover);
 	$Area2D.mouse_exited.connect(_Hover);
+	
+	$Area2D.area_entered.connect(_SIGNAL_Play_Collision_Sounds);
 
 
 func _input(event: InputEvent) -> void:
@@ -28,6 +31,8 @@ func _input(event: InputEvent) -> void:
 		$Area2D.mouse_entered.disconnect(_Hover);
 		$Area2D.mouse_exited.disconnect(_Hover);
 		
+		$Area2D.area_entered.disconnect(_SIGNAL_Play_Collision_Sounds);
+		
 		$Area2D.monitorable = false;
 		
 		_spr_array[0].hide();
@@ -35,12 +40,17 @@ func _input(event: InputEvent) -> void:
 		
 		# Sound
 		
-		match _play_sound:
-			AudioMaster.OBSTACLE_SOUND.NULL:
-				pass;
-			AudioMaster.OBSTACLE_SOUND.Leaf_Rustle:
-				AudioMaster.Play_Leaf_Rustle();
+		for sound in _touch_sounds:
+			AudioMaster.Play_By_Sound_Type(sound);
 
 
 func _Hover() -> void:
 	_hovering = !_hovering;
+
+
+func _SIGNAL_Play_Collision_Sounds(area:Area2D) -> void:
+	# TEMPORARY
+	if !area.get_parent().name.contains("Car"):
+		return;
+	for sound in _col_sounds:
+		AudioMaster.Play_By_Sound_Type(sound);
