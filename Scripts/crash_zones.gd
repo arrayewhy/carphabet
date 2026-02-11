21925e64fa15acd729095eff46b63c15cde8ce3b
Sundrop <random90s@gmail.com>
2026-02-11 13:36:02 +0800

    Added Sound handling to Crash Zones

--- Scripts/crash_zones.gd
+++ Scripts/crash_zones.gd
@@ -4,9 +4,18 @@ extends Area2D
 
 @export var _changes:Dictionary[int, Array];
 
+@export var _col_sounds:Array[AudioMaster.SOUND_TYPE] = [AudioMaster.SOUND_TYPE.Metal_Bang];
+
+
+# Functions: Built-in ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 
 func _ready() -> void:
 	$"..".Activate_Car.connect(_SIGNAL_Toggle_Areas);
+	self.area_entered.connect(_SIGNAL_Play_Collision_Sounds);
+
+
+# Functions: Signals ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 
 func _SIGNAL_Toggle_Areas(curr_car:int) -> void:
@@ -26,3 +35,11 @@ func _SIGNAL_Toggle_Areas(curr_car:int) -> void:
 			col_shape.hide();
 		else:
 			col_shape.show();
+
+
+func _SIGNAL_Play_Collision_Sounds(area:Area2D) -> void:
+	# TEMPORARY
+	if !area.get_parent().name.contains("Car"):
+		return;
+	for sound in _col_sounds:
+		AudioMaster.Play_By_Sound_Type(sound);
